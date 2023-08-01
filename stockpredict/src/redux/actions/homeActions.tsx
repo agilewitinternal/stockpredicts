bd265180a70b6ec01e8b278288fbd2f0032ed166
agilewitinternal <36783548+agilewitinternal@users.noreply.github.com>
2023-07-31 22:46:14 -0500

    Revert "reduxsetup"

--- stockpredict/src/redux/actions/homeActions.tsx
+++
@@ -1,8 +0,0 @@
-import { NEW } from "../actionTypes"
-
-export const setNewUser = (value: any) => {
-    return {
-        type: NEW,
-        payload: value
-    }
-}
--- stockpredict/src/redux/reducers/index.tsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import homeReducer from "./homeReducer"
-
-
-const appReducer = combineReducers({
-    home: homeReducer
-});
-
-export default appReducer
